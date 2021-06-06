93cad95515fc7fe9f19a5d57521361371652b62f
alamin <ribinaofficial018@gmail.com>
2021-06-06 15:33:24 +0600

    add admin dashboard

--- resources/views/layouts/backend/partial/leftsidebar.blade.php
+++ resources/views/layouts/backend/partial/leftsidebar.blade.php
@@ -42,18 +42,7 @@
                       
 
              <li class="header">System</li>
-                   <li>
-                        <a class="dropdown-item" href="{{ route('logout') }}"
-                           onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                            <i class="material-icons">layers</i>
-                            <span>Sign Out</span>
-                        </a>
-
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                            @csrf
-                        </form>
-                    </li>
+                   
 
              @endif
 
